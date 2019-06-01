a75184b87653b6f3394d2ef158b60dc0f30ae5b4
Nahuel Desimone <nahuel.desimone98@gmail.com>
2019-06-01 18:39:25 -0300

    Modifico el contenido de la clase asientoDTO

--- src/aterrizarv2/vuelos/AsientoDTO.java
+++ src/aterrizarv2/vuelos/AsientoDTO.java
@@ -3,7 +3,9 @@ package aterrizarv2.vuelos;
 
 import aterrizarv2.asientos.Asiento;
 import aterrizarv2.asientos.ClaseAsiento;
+import aterrizarv2.asientos.CodigoAsiento;
 import aterrizarv2.asientos.EnumUbicacionAsiento;
+import aterrizarv2.asientos.EstadoAsiento;
 import aterrizarv2.asientos.PrecioAsiento;
 import aterrizarv2.asientos.UbicacionAsiento;
 import aterrizarv2.fecha.FechaFlexible;
@@ -14,55 +16,24 @@ import aterrizarv2.hora.Hora;
 import aterrizarv2.hora.excepcionesHora.FormatoHoraIncorrectoException;
 import aterrizarv2.hora.excepcionesHora.HoraInvalidaException;
 
-public class AsientoDTO extends AsientoVueloFullData{
+public class AsientoDTO extends Asiento{
     
-    private String codigoVuelo;
-    private Integer numeroAsiento;
     private FechaFormatoLatinoamericano fechaSalida;
     private Hora horaSalida;
-    private PrecioAsiento precio;
-    private ClaseAsiento clase;
-    private UbicacionAsiento ubicacion;
-    
-    
-    public AsientoDTO(Asiento asiento, String origen, String destino, FechaFlexible fechaSal, Hora horaSal) throws FormatoFechaIncorrectoException, FechaNoValidaException, FormatoHoraIncorrectoException, HoraInvalidaException {
-        super(asiento, origen, destino, fechaSal, horaSal);
-        codigoVuelo = asiento.getCodigo().getCodigo();
-        numeroAsiento = Integer.parseInt(asiento.getCodigo().getNumeroAsiento());
-        fechaSalida = new FechaFormatoLatinoamericano(fechaSal.representacionEnLatinoamericano());
-        horaSalida = new Hora(horaSal.getHoraFormatoString());
-        precio = asiento.getPrecio();
-        clase = asiento.getClase();
-        ubicacion = asiento.getUbicacion();
-    }
 
-    public String getCodigoVuelo() {
-        return codigoVuelo;
-    }
-
-    public Integer getNumeroAsiento() {
-        return numeroAsiento;
+    public AsientoDTO(FechaFormatoLatinoamericano fechaSalida, Hora horaSalida, ClaseAsiento clase, CodigoAsiento codigo, EstadoAsiento estado, PrecioAsiento precio, UbicacionAsiento ubicacion) {
+        super(clase, codigo, estado, precio, ubicacion);
+        
+        this.fechaSalida = fechaSalida;
+        this.horaSalida = horaSalida;
     }
 
     public FechaFormatoLatinoamericano getFechaSalidaDTO() {
         return fechaSalida;
     }
-
-    @Override
+    
     public Hora getHoraSalida() {
         return horaSalida;
     }
-
-    public PrecioAsiento getPrecio() {
-        return precio;
-    }
-
-    public ClaseAsiento getClase() {
-        return clase;
-    }
-
-    public UbicacionAsiento getUbicacion() {
-        return ubicacion;
-    }
-
+    
 }
